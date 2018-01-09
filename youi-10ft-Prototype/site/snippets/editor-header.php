40850a9f6978e97c87354f0fc5311e2a198ca991
Matthew Ernst <matternst@MattEs-MacBook-Pro.local>
2018-01-08 20:12:04 -0500

    Minor edits to css

--- youi-10ft-Prototype/site/snippets/editor-header.php
+++
@@ -1,9 +0,0 @@
-<header class="header cf editor-header" role="banner">
-
-	<h5><span class="prototype-main-header"><?php echo $page->title()->html() ?></span></h5>
-
-
-	<!--<h5><?php echo html($page->parent()->title()) ?> Prototype: <a href="" class="tag-menu"><?php echo $page->title()->html() ?></a>  | <a href="" class="info-button release-notes">Release notes</a> </h5>-->
-	
-	<h5 class="current-screen"><!-- <span class="screen-title"></span> screen <a href="" class="info-button screen-info-btn">screen info</a> |  --><a href="" class="screen-menu">ALL SCREENS</a> <a href="" class="info-button full-screen-btn">&#9859;</a></h5>
-</header> 
